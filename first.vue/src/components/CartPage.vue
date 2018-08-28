500c4ca3c3d9335cbb66a4aa3a388fdd7c7e7b46
student <student@GFL-1465.ad.geeksforless.net>
2018-08-28 20:45:39 +0300

    filter completed

--- first.vue/src/components/CartPage.vue
+++ first.vue/src/components/CartPage.vue
@@ -2,17 +2,23 @@
     <div>
         <h1>Cart Page</h1>
         <ul>
-            <li v-for="(item, index) in list" :key="index">{{ index }} - {{ item.count }}</li>
+            <li v-for="(item, index) in list" :key="index">{{ index | titleFilter }} - {{ item.count }}</li>
         </ul>
     </div>
 </template>
 <script>
 import cart from '../store/cart';
 import products from '../store/products';
+
 export default {
     computed: {
         list() {
             return cart.products;
+        }
+    },
+    filters:{
+        titleFilter: function(index){
+           return products.list[index].title;
         }
     }
 }
