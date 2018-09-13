af3e75c45eff06ce65be5d9c923e874fa689bf94
paulr-gflcourses <paulgerald123@gmail.com>
2018-09-13 16:18:51 +0300

    game squares is ready!

--- Node/squaresGame/index.js
+++ Node/squaresGame/index.js
@@ -14,40 +14,45 @@ server.listen(5000);
 
 var clients = {};
 
-var messages = require('./data/messages');
+var messages = {};
 var counter = 0;
-var wss = new ws.Server({port: 5555});
+var wss = new ws.Server({
+    port: 5555
+});
 
 wss.on('connection', (wsc, request) => {
     console.log(request.headers.cookie);
-    // console.log(request);
     let id = counter++;
     clients[id] = wsc;
     wsc.on('message', (message) => {
-        message=JSON.parse(message);
-        messages.push({username: message.username, message:message.message});
+
+        message = JSON.parse(message);
+        messages[id] = {
+            x: message.x,
+            y: message.y,
+            color: message.color
+        };
         for (let cid in clients) {
             let client = clients[cid];
             client.send(JSON.stringify({
-                type: 'message',
-                message: message.message, 
-                username: message.username
+                type: 'messages',
+                messages: messages
             }));
         }
-        /* 
-            wss.clients.forEach((client) => {
-                client.send(JSON.stringify({
-                    type: 'message',
-                    message
-                }));
-            })
-        */
+
     });
 
     wsc.on('close', () => {
         console.log('connect close');
-        // clearInterval(timer);
         delete clients[id];
+        delete messages[id];
+        for (let cid in clients) {
+            let client = clients[cid];
+            client.send(JSON.stringify({
+                type: 'messages',
+                messages: messages
+            }));
+        }
     })
 
     wsc.send(JSON.stringify({
@@ -55,31 +60,4 @@ wss.on('connection', (wsc, request) => {
         messages
     }));
 
-
-    /* let timer = setInterval(() => {
-        wsc.send(JSON.stringify({
-            type: 'memoryInfo',
-            data: process.memoryUsage()
-        }))
-    }, 200) */
-
-    // Example disconnect
-    /* setTimeout(() => {
-        wsc.close()
-    }, 5000) */
 })
-
-setInterval(() => {
-    fs.writeFile('./data/messages.json', JSON.stringify(messages), (err) => {if (err) console.log('error',err)});
-}, 1000);
-
-// var tt = new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//         reject('some bad');
-//         resolve('ok')
-//     }, 5000)
-// })
-
-// tt.then((ok) => {console.log(ok)})
-// .catch(() => {console.log('error')})
-// .then(() => {console.log('always')})
